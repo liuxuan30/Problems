27361549a8bbd2f59c6bdc42a59a488e81ecff2b
Xuan <xuan@Xuans-Tiny-MacBook-Pro.local>
2016-03-02 09:05:32 +0800

    oj

---
+++ OCOJ/OCOJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCOJ
+//
+//  Created by Xuan on 3/2/16.
+//  Copyright © 2016 Wingzero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCOJ/OCOJ/Solution.swift
@@ -0,0 +1,9 @@
+//
+//  Solution.swift
+//  OCOJ
+//
+//  Created by Xuan on 3/2/16.
+//  Copyright © 2016 Wingzero. All rights reserved.
+//
+
+import Foundation
